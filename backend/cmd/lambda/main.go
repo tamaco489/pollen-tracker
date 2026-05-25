d15726537833e465402d1ce94a0e27a676ec9e7c
tamaco489 <tamaco489@gmail.com>
2026-05-25 22:27:09 +0900

    feat: internal/server パッケージを追加し main.go を簡素化

--- backend/cmd/lambda/main.go
+++ backend/cmd/lambda/main.go
@@ -4,24 +4,16 @@ import (
 	"context"
 	"os"
 
-	"github.com/labstack/echo/v5"
-	"github.com/tamaco489/pollen-tracker/backend/internal/gen"
-	"github.com/tamaco489/pollen-tracker/backend/internal/handler"
+	"github.com/tamaco489/pollen-tracker/backend/internal/server"
 	"github.com/tamaco489/pollen-tracker/backend/pkg/logger"
 )
 
 func main() {
 	ctx := context.Background()
-
 	l := logger.New()
-
-	e := echo.New()
-
-	h := handler.New(l)
-	gen.RegisterHandlers(e, gen.NewStrictHandler(h, nil))
-
+	s := server.New(l)
 	l.InfoContext(ctx, "starting server", "addr", ":8080")
-	if err := e.Start(":8080"); err != nil {
+	if err := s.Run(ctx); err != nil {
 		l.ErrorContext(ctx, "server failed", "error", err)
 		os.Exit(1)
 	}
